6f6e058b5cb1ed65ad6aa39080bfcad59aabc4d0
pittsblake <pittsblake1@gmail.com>
2017-11-02 19:41:06 -0400

    styled cityList page

--- client/src/components/CityList.js
+++ client/src/components/CityList.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import axios from 'axios'
 
+
 const CityListStyle = styled.div`
     h1 {
         text-align: center;
@@ -17,35 +18,56 @@ justify-content: center;
 
 const CityPhoto = styled.div`
       img {
-          height: 100px;
-          width: 100px;
+          height: 300px;
+          width: 300px;
           margin: 0px auto;
           display: flex;
-          border-radius: 50%;
-          padding: 0px 50px 10px 50px;
+          border-radius: 50%; 
+          padding: 0px 50px 0px 50px;
+          width: 300px;
+          &:hover {
+              height: 320px;
+              width: 320px;
+              border-radius: 50%
+          }
       }
       text-align: center;
-    
+`
+
+const ATag = styled.div`
+    a {
+        text-decoration: none;
+        color: #7e6d4e;
+        font-size: 30px;
+        font-family: 'Bowlby One SC', cursive;
+        &:hover {
+            border-bottom: solid thick;
+            border-bottom-color: #7e6d4e;
+            text-shadow: 2px 3px 6px #39CCCC
+        }
+    }
+    padding: 20px
 `
 
 
 const CityList = (props) => {
-        return (
-            <CityListStyle>
-                <h1> All Cites </h1>
-                <CityLink>
-                    {props.cities.map(city => (
-                        <div key={city.id}> 
-                            <CityPhoto>
-                            <img src={city.image} alt={city.name + "City Photo"}/>
-                            <Link to={`/cities/${city.id}`}> {city.name} </Link>
-                            </CityPhoto>
-                        </div>
+    return (
+        <CityListStyle>
+            <CityLink>
+                {props.cities.map(city => (
+                    <div key={city.id}>
+                        <CityPhoto>
+                            <a href={`/cities/${city.id}`}><img src={city.image} alt={city.name + "City Photo"} /> </a>
+                            <ATag>
+                                <a href={`/cities/${city.id}`}> {city.name} </a>
+                            </ATag>
+                        </CityPhoto>
+                    </div>
 
-                    ))}
-                </CityLink>
-            </CityListStyle>
-        );
+                ))}
+            </CityLink>
+        </CityListStyle>
+    );
 }
 
 export default CityList;
